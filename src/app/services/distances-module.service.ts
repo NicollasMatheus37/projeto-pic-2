2a641d4c9a7e1c40bb1cec0d98505d8295db1495
Nicollas Matheus <nicollasmatheus37@gmail.com>
2018-11-05 09:16:14 -0200

    exemplos de variáveis a serem adicionadas em distances e districts

--- src/app/services/distances-module.service.ts
+++ src/app/services/distances-module.service.ts
@@ -5,6 +5,13 @@ export class DistancesModuleService {
 
   protected name = "distances";
 
+  /* exemplo de variável a ser adicionada */
+  private distances = [ /* objeto */
+    {inicio: "", destino: "", distance: ""}, /* início: bairro 1 - destino: bairro 2 - distance: distancia entre eles */
+    {inicio: "", destino: "", distance: ""},
+    {inicio: "", destino: "", distance: ""}
+  ]
+
   constructor() { }
 
 }
--- src/app/services/districts-module.service.ts
+++ src/app/services/districts-module.service.ts
@@ -5,6 +5,14 @@ export class DistrictsModuleService {
 
   protected name = "districts";
 
+  /* exemplo de variável a ser adicionada */
+  private centro: String = ""; /* variáveis com camelCase, tipadas e inciadas */
+  private cruzeiroDoSul: String = "";
+
   constructor() { }
+
+  getDistrict() {
+    
+  }
 
 }
